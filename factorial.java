e3c82264ae5321f6a8f8f1b2029f614d753079c2
jamesmuo <26312885+jamesmuo@users.noreply.github.com>
2021-04-19 22:57:34 -0500

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,34 @@
+public class Factorial {
+  Interface IFactorial {
+    public int factorial(int n);
+  }
+  
+  class FactorialRecursion implements IFactorial {
+    public int factorial(int n) {
+      if (n <= 1) return 1;
+      else return n * factorial(n - 1);
+    }
+  }
+  
+  class FactorialIteration implements IFactorial {
+    public int factorial(int n) {
+      int fact = 1;
+      
+      for (int i = n; i > 0; ++i) {
+        fact = fact * i;
+      }
+      return fact;
+    }
+  }  
+  
+  public static void main(String[] args) {
+    Factorial fr = null, fi = null;
+    int n = 5;
+    
+    fr = new FactorialRecursion();
+    fi = new FactorialIteration();
+    
+    System.out.println("Factorial (Recursion) of " + n + " = " + fr.factorial(n));
+    System.out.println("Factorial (Iteration) of " + n + " = " + fi.factorial(n));
+  }
+}
