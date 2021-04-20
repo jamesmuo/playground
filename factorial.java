93d4f64bc736b09d7e791d1b62c02119d4795252
jamesmuo <26312885+jamesmuo@users.noreply.github.com>
2021-04-19 23:19:52 -0500

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -14,7 +14,7 @@ public class Factorial {
     public int factorial(int n) {
       int fact = 1;
       
-      for (int i = n; i > 0; ++i) {
+      for (int i = n; i > 0; --i) {
         fact = fact * i;
       }
       return fact;
